170538af92ee445b145dbc320c0c53067900ac35
Bohredd <dapereira@inf.ufsm.br>
2024-12-26 10:38:21 -0300

    feat: more types set

--- django_react_types/types.py
+++ django_react_types/types.py
@@ -11,5 +11,28 @@ FIELD_TYPE_MAPPING = {
     models.DecimalField: "number",
     models.ForeignKey: "string",  # You may want to handle relationships differently
     models.ManyToManyField: "string[]",  # Similar handling for many-to-many relationships
-    # Add other mappings here
+    models.AutoField: "number", # id 
+    models.PositiveIntegerField: "number",
+    models.PositiveSmallIntegerField: "number",
+    models.EmailField: "string",
+    models.PositiveBigIntegerField: "number",
+    models.BinaryField: "string",
+    models.BigAutoField: "number",
+    models.BigIntegerField: "number",
+    models.DurationField: "string",
+    models.GenericIPAddressField: "string",
+    models.IPAddressField: "string",
+    models.SmallIntegerField: "number",
+    models.SlugField: "string",
+    models.TimeField: "string",
+    models.URLField: "string",
+    models.UUIDField: "string",
+    models.FileField: "string",
+    models.ImageField: "string",
+    models.FilePathField: "string",
+    models.JSONField: "string",
+    models.NullBooleanField: "boolean",
+    models.SmallAutoField: "number",
+    models.SmallIntegerField: "number",
+    models.TimeField: "string",
 }
